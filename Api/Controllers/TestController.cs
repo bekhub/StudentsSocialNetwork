7317e76de9da37c44c89d96b6602a5af0f1c2fd2
Bekjan <bekambkg@gmail.com>
2021-03-07 22:04:47 +0600

    Initial commit

---
+++ Api/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string HelloWorld() => "Hello World";
+    }
+}

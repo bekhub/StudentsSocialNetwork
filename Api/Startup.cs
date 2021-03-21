f5ae7a4d19ce470c1bde7446e86068a8abfb428e
Bekjan <bekambkg@gmail.com>
2021-03-21 17:10:38 +0600

    feature/SSN-15

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,37 +1,52 @@
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Infrastructure;
 
 namespace Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private const string CORS_POLICY = "CorsPolicy";
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            _environment = environment;
         }
 
         public IConfiguration Configuration { get; }
 
+        private readonly IWebHostEnvironment _environment;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CORS_POLICY, builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+            services.AddDbContext(Configuration.GetConnectionString("DefaultConnection"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SSNBackend", Version = "v1" });
+                c.EnableAnnotations();
             });
+        }
+
+        public void ConfigureContainerBuilder(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new DefaultInfrastructureModule());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,15 +56,22 @@ namespace Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
             
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SSNBackend v1"));
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(CORS_POLICY);
+
+            app.UseAuthentication();
             app.UseAuthorization();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SSNBackend v1"));
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Infrastructure/DefaultInfrastructureModule.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Autofac;
+using Core;
+using Core.Interfaces;
+using Infrastructure.Data;
+using MediatR;
+using MediatR.Pipeline;
+using Module = Autofac.Module;
+
+namespace Infrastructure
+{
+    public class DefaultInfrastructureModule : Module
+    {
+        private readonly List<Assembly> _assemblies = new();
+
+        public DefaultInfrastructureModule(Assembly callingAssembly = null)
+        {
+            var coreAssembly = Assembly.GetAssembly(typeof(CoreAssembly));
+            var infrastructureAssembly = Assembly.GetAssembly(typeof(EfRepository<>));
+            _assemblies.Add(coreAssembly);
+            _assemblies.Add(infrastructureAssembly);
+            if (callingAssembly != null)
+            {
+                _assemblies.Add(callingAssembly);
+            }
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            RegisterCommonDependencies(builder);
+        }
+
+        private void RegisterCommonDependencies(ContainerBuilder builder)
+        {
+            builder.RegisterType(typeof(EfRepository<>)).As(typeof(IRepository<>))
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<Mediator>()
+                .As<IMediator>()
+                .InstancePerLifetimeScope();
+
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            var mediatrOpenTypes = new[]
+            {
+                typeof(IRequestHandler<,>),
+                typeof(IRequestExceptionHandler<,,>),
+                typeof(IRequestExceptionAction<,>),
+                typeof(INotificationHandler<>),
+            };
+
+            foreach (var mediatrOpenType in mediatrOpenTypes)
+            {
+                builder
+                .RegisterAssemblyTypes(_assemblies.ToArray())
+                .AsClosedTypesOf(mediatrOpenType)
+                .AsImplementedInterfaces();
+            }
+
+            builder.RegisterType<EmailSender>().As<IEmailSender>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
